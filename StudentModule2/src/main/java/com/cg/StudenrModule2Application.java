abb3c24f1a58ed1b9d1424a4046a6e7702b412c9
rkuma530 <rkuma530@INLEN8520041989.corp.capgemini.com>
2022-03-16 11:20:06 +0530

    Student Module2 Created

---
+++ StudentModule2/src/main/java/com/cg/StudenrModule2Application.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenrModule2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenrModule2Application.class, args);
+	}
+
+}

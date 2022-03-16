7c7e7f69601f4fab9b26ae27490989e69c0ee5a9
Meet280399 <74697242+Meet280399@users.noreply.github.com>
2022-03-16 22:22:12 +0530

    Delete StudentModule1 directory

--- StudentModule1/src/main/java/com/cg/dao/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.entity.Student;
-
-public interface FeedbackRepository  extends JpaRepository<Student, Integer>{
-
-}

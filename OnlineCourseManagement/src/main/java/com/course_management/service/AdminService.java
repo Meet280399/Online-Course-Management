58f6381a9d506430bda00964f702e08ffaacba0c
Jonny <meett.patel@avm.edu.in>
2022-03-17 18:12:48 +0530

    Solved error and made application running .... errors were in entities and in the service package.

--- OnlineCourseManagement/src/main/java/com/course_management/service/AdminService.java
+++ OnlineCourseManagement/src/main/java/com/course_management/service/AdminService.java
@@ -1,7 +1,5 @@
 package com.course_management.service;
 
-
-
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,11 +7,8 @@ import org.springframework.stereotype.Service;
 import com.course_management.entity.Instructor;
 import com.course_management.entity.Student;
 
-
-
 @Service
-public interface AdminService
-{
+public interface AdminService {
 //public List<String> setCurriculum();
 //public List<Subject> getAllSubjects();
 //public List<Subject> deleteSubject(Integer subjectId);
@@ -21,30 +16,41 @@ public interface AdminService
 //public List<Student> updateSubject(Subject Subject);
 //public Subject findSubject(Integer SubjectId);
 
-
-
 //Administrator has managing the Students
-public List<Student> getallStudents();
-public List<Student> deleteStudent(Integer studentId);
-public List<Student> saveStudent(Student student);
-public List<Student> updateStudent(Student student);
-public Student findStudent(Integer studentId);
+	public List<Student> getallStudents();
+
+	public List<Student> deleteStudent(Integer studentId);
+
+	public List<Student> saveStudent(Student student);
+
+	public List<Student> updateStudent(Student student);
+
+	public Student findStudent(Integer studentId);
 
 //Administrator has managing the Instructor
-public List<Instructor> getallInstructors();
-public List<Instructor> deleteInstructor(Integer instructorId);
-public List<Instructor> saveInstructor(Instructor instructor);
-public List<Instructor> updateInstructor(Instructor instructor);
-public Instructor findInstructor(Integer instructorId);
+	public List<Instructor> getallInstructors();
+
+	public List<Instructor> deleteInstructor(Integer instructorId);
+
+	public List<Instructor> saveInstructor(Instructor instructor);
+
+	public List<Instructor> updateInstructor(Instructor instructor);
+
+	public Instructor findInstructor(Integer instructorId);
 //Administrator dealing the Salary of Instructor
 
-public List<Instructor> setsalaryToInstructor(double instructorSalary);
-public List<Instructor> getallSalaryOfInstructor(double instructorSalary);
-public List<Instructor> deleteSalaryOfInstructor(double instructorSalary);
-public List<Instructor> updatesalaryofInstructor(double instructorSalary);
-public List<Instructor> savesalaryofInstructor(double instructorSalary);
-public List<Instructor> saveAll();
+	public List<Instructor> setsalaryToInstructor(double instructorSalary);
 
-public double instructorPracticalHours();
+	public List<Instructor> getallSalaryOfInstructor(double instructorSalary);
+
+	public List<Instructor> deleteSalaryOfInstructor(double instructorSalary);
+
+	public List<Instructor> updatesalaryofInstructor(double instructorSalary);
+
+	public List<Instructor> savesalaryofInstructor(double instructorSalary);
+
+	public List<Instructor> saveAll();
+
+	public double instructorPracticalHours();
 
 }

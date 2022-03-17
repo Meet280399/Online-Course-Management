79708cad9a69ee1e97034f7aacf7a0ae7118b06d
rkuma530 <rkuma530@INLEN8520041989.corp.capgemini.com>
2022-03-17 12:30:34 +0530

    Created relation between course and  project entity(One to many)

--- OnlineCourseManagement/src/main/java/com/course_management/entity/Course.java
+++ OnlineCourseManagement/src/main/java/com/course_management/entity/Course.java
@@ -12,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -30,55 +31,66 @@ private int courseDuration;
 @JoinTable(name = "Course_Subject", joinColumns = { @JoinColumn(name = "course_id") }, inverseJoinColumns = { @JoinColumn(name = "subject_id") })
 private Set<Subject> subjects = new HashSet<>();
 
+ @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+ private Set<Project> projects = new HashSet<>();
+ 
  public Course() {
 }
 
- public Course(int courseId, String courseName, int courseDuration, Set<Subject> subjects) {
-this.courseId = courseId;
-this.courseName = courseName;
-this.courseDuration = courseDuration;
-this.subjects = subjects;
+public Course(int courseId, String courseName, int courseDuration, Set<Subject> subjects, Set<Project> projects) {
+	super();
+	this.courseId = courseId;
+	this.courseName = courseName;
+	this.courseDuration = courseDuration;
+	this.subjects = subjects;
+	this.projects = projects;
 }
 
- public int getCourseId() {
-return courseId;
+public int getCourseId() {
+	return courseId;
 }
 
- public void setCourseId(int courseId) {
-this.courseId = courseId;
+public void setCourseId(int courseId) {
+	this.courseId = courseId;
 }
 
- public String getCourseName() {
-return courseName;
+public String getCourseName() {
+	return courseName;
 }
 
- public void setCourseName(String courseName) {
-this.courseName = courseName;
+public void setCourseName(String courseName) {
+	this.courseName = courseName;
 }
 
- public int getCourseDuration() {
-return courseDuration;
+public int getCourseDuration() {
+	return courseDuration;
 }
 
- public void setCourseDuration(int courseDuration) {
-this.courseDuration = courseDuration;
+public void setCourseDuration(int courseDuration) {
+	this.courseDuration = courseDuration;
 }
 
- public Set<Subject> getSubjects() {
-return subjects;
+public Set<Subject> getSubjects() {
+	return subjects;
 }
 
- public void setSubjects(Set<Subject> subjects) {
-this.subjects = subjects;
+public void setSubjects(Set<Subject> subjects) {
+	this.subjects = subjects;
 }
 
- @Override
+public Set<Project> getProjects() {
+	return projects;
+}
+
+public void setProjects(Set<Project> projects) {
+	this.projects = projects;
+}
+
+@Override
 public String toString() {
-return "Course [courseId=" + courseId + ", courseName=" + courseName + ", courseDuration=" + courseDuration
-+ ", subjects=" + subjects + "]";
-}
-public void addSubject(Subject subject) {
-this.getSubjects().add(subject);
+	return "Course [courseId=" + courseId + ", courseName=" + courseName + ", courseDuration=" + courseDuration
+			+ ", subjects=" + subjects + ", projects=" + projects + "]";
 }
 
+ 
 }
--- OnlineCourseManagement/src/main/java/com/course_management/entity/Project.java
+++ OnlineCourseManagement/src/main/java/com/course_management/entity/Project.java
@@ -8,62 +8,75 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.ManyToAny;
 
 @Entity
 @Table(name="project")
-public class ProjectManagement {
+public class Project {
 	@Id
 	@GeneratedValue
 	@Column(name="project_Id")
-	@ManyToMany(fetch=FetchType.LAZY,mappedBy="projects")
-	private Set<Course> courses=new HashSet<>();
 	private int projectId;
 	private String projectName;
 	private String projectDifficultyLevel;
-	public ProjectManagement() {
+	
+	@ManyToOne
+	@JoinColumn(name="course_id")
+	private Course course;
+	
+	public Project() {
 		
 	}
-	public ProjectManagement(Set<Course> courses, int projectId, String projectName, String projectDifficultyLevel) {
+
+	public Project(int projectId, String projectName, String projectDifficultyLevel, Course course) {
 		super();
-		this.courses = courses;
 		this.projectId = projectId;
 		this.projectName = projectName;
 		this.projectDifficultyLevel = projectDifficultyLevel;
+		this.course = course;
 	}
-	public Set<Course> getCourses() {
-		return courses;
-	}
-	public void setCourses(Set<Course> courses) {
-		this.courses = courses;
-	}
+
 	public int getProjectId() {
 		return projectId;
 	}
+
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
+
 	public String getProjectName() {
 		return projectName;
 	}
+
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
+
 	public String getProjectDifficultyLevel() {
 		return projectDifficultyLevel;
 	}
+
 	public void setProjectDifficultyLevel(String projectDifficultyLevel) {
 		this.projectDifficultyLevel = projectDifficultyLevel;
 	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
 	@Override
 	public String toString() {
-		return "ProjectManagement [courses=" + courses + ", projectId=" + projectId + ", projectName=" + projectName
-				+ ", projectDifficultyLevel=" + projectDifficultyLevel + "]";
+		return "Project [projectId=" + projectId + ", projectName=" + projectName + ", projectDifficultyLevel="
+				+ projectDifficultyLevel + ", course=" + course + "]";
 	}
 	
-	
-
 }

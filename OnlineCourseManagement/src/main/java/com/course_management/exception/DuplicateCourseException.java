09e82cff98bfa77ae273d28c1170ff754876f455
Jonny <meett.patel@avm.edu.in>
2022-03-18 11:48:35 +0530

    Added methods for the exceptions classes DuplicateCourseException, InstructorNotFoundException, InvalidLoginCredentials, StudentNotFound and SubjectNotFound.

--- OnlineCourseManagement/src/main/java/com/course_management/exception/DuplicateCourseException.java
+++ OnlineCourseManagement/src/main/java/com/course_management/exception/DuplicateCourseException.java
@@ -2,4 +2,17 @@ package com.course_management.exception;
 
 public class DuplicateCourseException extends Exception {
 
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public DuplicateCourseException(String message) {
+		super(message);
+	}
 }

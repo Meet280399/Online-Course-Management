27454eaed5654dc4efd39c703f63336e16c0a6c3
Jonny <meett.patel@avm.edu.in>
2022-03-18 09:16:39 +0530

    Created the functionalities for the feedback entity

--- OnlineCourseManagement/src/main/java/com/course_management/service/InstructorService.java
+++ OnlineCourseManagement/src/main/java/com/course_management/service/InstructorService.java
@@ -4,14 +4,24 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.course_management.entity.Feedback;
 import com.course_management.entity.Instructor;
 
 @Service
 public interface InstructorService {
+	
+	// methods for managing the instructor
 	public List<Instructor> getAllInstructors();
 	public List<Instructor> deleteInstructor(Integer instructorId);
 	public Instructor saveInstructor(Instructor instructor);
 	public List<Instructor> updateInstructor(Instructor instructor);
 	public Instructor findInstructor(Integer instructorId);
 
+	
+	//method for managing feedbacks given by the Instructor
+	public List<Feedback> getAllFeedbacks();
+	public List<Feedback> deleteFeedback(Integer feedbackId);
+	public Feedback saveFeedback(Feedback feedback);
+	public List<Feedback> updateFeedback(Feedback feedback);
+	public Feedback findFeedback(Integer feedbackId);
 }

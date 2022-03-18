b5ff17990fa6585c2d9d854649ef7260f5fa31e3
Jonny <meett.patel@avm.edu.in>
2022-03-18 11:56:10 +0530

    removed push head error from feedback exception

--- OnlineCourseManagement/src/main/java/com/course_management/exception/NoSuchFeedbackException.java
+++ OnlineCourseManagement/src/main/java/com/course_management/exception/NoSuchFeedbackException.java
@@ -2,13 +2,10 @@ package com.course_management.exception;
 
 public class NoSuchFeedbackException extends Exception {
 	
-<<<<<<< HEAD
-=======
 	public String message;
 	
 	public NoSuchFeedbackException(String message) {
 		super(message);
 	}
->>>>>>> branch 'main' of https://github.com/Meet280399/Online-Course-Management.git
 
 }

2c930e52c6e61cead7cabebe1d423453f2fdf667
91636 <91636@LAPTOP-V654183S>
2022-03-18 11:50:04 +0530

    modified nosuchfeedback exception class

--- OnlineCourseManagement/src/main/java/com/course_management/exception/NoSuchFeedbackException.java
+++ OnlineCourseManagement/src/main/java/com/course_management/exception/NoSuchFeedbackException.java
@@ -1,5 +1,11 @@
 package com.course_management.exception;
 
 public class NoSuchFeedbackException extends Exception {
+	
+	public String message;
+	
+	public NoSuchFeedbackException(String message) {
+		super(message);
+	}
 
 }
